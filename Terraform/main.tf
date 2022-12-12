d9c3a8785615a8956a39066b4096053906ff47fe
keerthireddy112021 <120305795+keerthireddy112021@users.noreply.github.com>
2022-12-12 23:02:57 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "Resourcegroup" {
+  name     = "Resourcegmvccroup"
+  location = "East US"
+}
 
